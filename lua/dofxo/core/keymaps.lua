4bd5f92ed07913d6b65e4cc529f262d1b892e623
dofxo <kargat504@gmail.com>
2024-05-13 21:17:20 +0330

    Feat: add redo action remap

--- lua/dofxo/core/keymaps.lua
+++ lua/dofxo/core/keymaps.lua
@@ -12,6 +12,9 @@ keymap.set("n", "<leader><leader>", vim.cmd.w, { desc = "works as :w to save fil
 -- clear search highlights
 keymap.set("n", "<leader>cl", ":nohl<CR>", { desc = "Clear search highlights" })
 
+-- redo action
+keymap.set("n", "<leader>r", vim.cmd.redo, { desc = "Redo action" })
+
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decr ment number" }) -- decrement
